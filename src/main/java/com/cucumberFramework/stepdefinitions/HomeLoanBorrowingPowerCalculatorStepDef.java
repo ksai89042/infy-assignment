25eb547f6e4d930a56f43f94033adc5ebc6c025d
Narasimha <narasimha.dontula@gmail.com>
2022-09-08 20:04:09 +0530

    file name changes

--- src/main/java/com/cucumberFramework/stepdefinitions/HomeLoanBorrowingPowerCalculatorStepDef.java
+++ src/main/java/com/cucumberFramework/stepdefinitions/HomeLoanBorrowingPowerCalculatorStepDef.java
@@ -10,7 +10,7 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import junit.framework.Assert;
 
-public class HomeLoanPageStepDefinitions extends TestBase {
+public class HomeLoanBorrowingPowerCalculatorStepDef extends TestBase {
 
 	HomeLoanBorrowingPowerCalculatorPage homeLoanBorrowingPowerCalculatorPage = new HomeLoanBorrowingPowerCalculatorPage(driver);
 	WaitHelper waitHelper = new WaitHelper(driver);
